4c81144f372ffa596f645aeca9920c79cc22bba5
lmg <1138075190@qq.com>
2021-12-22 09:44:50 +0800

    提交信息

--- dist.sh
+++ dist.sh
@@ -4,40 +4,24 @@
 set -e
 
 # 生成静态文件
-# npm run build
+npm run build
 
 # 进入生成的文件夹
-# if [ ! -d .git]; then
-	git init
-# fi
-
+git init
 git add .
-echo "請輸入躰驕傲信息"
-read info
-git commit -m '$info'
+git commit -m '提交信息'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-if [ ! -d .git]; then
-	git push -f git@github.com:github15238484509/learn.git master
-else
-	git push
-fi
+git push -f git@github.com:github15238484509/learn.git master
 
 cd docs/.vuepress/dist
 
-if [ ! -d .git]; then
-	git init
-fi
+git init
 
 git add .
-git commit -m '$info'
+git commit -m '提交信息'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-
-if [ ! -d .git]; then
-	git push -f git@github.com:github15238484509/learn.git master:document
-else
-	git push
-fi
+git push -f git@github.com:github15238484509/learn.git master:document
 
 cd -
